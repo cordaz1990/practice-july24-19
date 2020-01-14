954770dcf6b4270944236e5c0e1e3912294eb687
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 21:05:41 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,18 @@
-print('hello')
-print('world')
-p
+word = 'banana'
+count = 0 
+for letter in word:
+  if letter == 'a':
+     count = count + 1
+print(count)
+
+
+word = 'banana'
+index = word.find('a')
+index 
+1
+
+def in_both(word1,word2):
+    for letter in word1:
+        if letter in word2:
+           print(letter)
+            
